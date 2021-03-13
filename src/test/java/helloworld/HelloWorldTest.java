2ca7591840e68b20d718797cc517bb0b0adedc84
cevataykans <aykansevinc66@gmail.com>
2021-03-13 14:35:40 +0300

    Create simple maven project.

---
+++ src/test/java/helloworld/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World", "Hello World");
+    }
+}
