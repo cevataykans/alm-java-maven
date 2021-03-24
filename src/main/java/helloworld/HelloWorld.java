19435dfeb4a2f5248ebad77ed62bcbdf0b1bd33c
bucketheaded <ismailssahal@gmail.com>
2021-03-24 22:44:54 +0300

    DEMO FOR REVIEW

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,13 +3,13 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World, Maven");
-
+        System.out.println("Hello World, Maven, please review me");
+		
         System.out.println(mavenResponder());
-
+		System.out.println("Thank you for the review");
     }
 
     public static String mavenResponder(){
-        return "Hi, nice to meet you";
+        return "Hi, nice to meet you, I will review you";
     }
 }
