8562ef0b6a7ef49155123012a49fa42293d47431
bucketheaded <ismailssahal@gmail.com>
2021-03-28 15:18:00 +0300

    It is time to finish this review

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -6,10 +6,9 @@ public class HelloWorld {
         System.out.println("Hello World, Maven, please review me");
 		
         System.out.println(mavenResponder());
-		System.out.println("Thank you for the review");
     }
 
     public static String mavenResponder(){
-        return "Hi, nice to meet you, I will review you";
+        return "Hi, nice to meet you, I have finished my review";
     }
 }
