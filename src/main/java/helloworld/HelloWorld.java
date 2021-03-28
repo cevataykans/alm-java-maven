9d1588ef646d979543db04a2840b85b97f854d11
bucketheaded <ismailssahal@gmail.com>
2021-03-28 15:34:27 +0300

    wont fail now

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,12 +3,12 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World, Maven, please review me");
+        System.out.println("Hello World");
 		
         System.out.println(mavenResponder());
     }
 
     public static String mavenResponder(){
-        return "Hi, nice to meet you, I have finished my review";
+        return "Hi, nice to meet you";
     }
 }
